583173e902323542094c0d157016627ba82eef2f
Anastasiia Bratochkina <abratochkina@gmail.com>
2017-05-07 15:25:36 +0300

    Add reducer for 'generations counter' container

---
+++ src/reducers/reducer-generations-counter.js
@@ -0,0 +1,14 @@
+const generationsCounter = (state = 0, action) => {
+  switch (action.type) {
+    case 'SPEED_CONTROL':
+      return state + 1;
+    case 'MAKE_RANDOM':
+      return 0;
+    case 'CLEAR':
+      return 0;
+    default:
+    return state;
+  }
+};
+
+export default generationsCounter;
